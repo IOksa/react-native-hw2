a2528da59cbbffbe4bb417be379d035bf5fc089d
IOksa <117656767+IOksa@users.noreply.github.com>
2023-08-10 19:02:17 +0300

    update

--- src/components/UserAvatar.jsx
+++ src/components/UserAvatar.jsx
@@ -1,15 +1,30 @@
-import React from "react";
-import {View, TouchableOpacity, Image, StyleSheet, Alert} from "react-native";
+import React, { useState} from 'react';
+import {View, TouchableOpacity, Image, StyleSheet} from "react-native";
 import { AntDesign } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker';
 
 const UserAvatar=()=>{
-    let isLoadFoto=false;
+    const [image, setImage] = useState(null);
+
+    const pickImage = async () => {
+        // No permissions request is necessary for launching the image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.All,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+       
+        if (!result.canceled) {
+          setImage(result.assets[0].uri);
+        }
+      };
    
 return (
     <View style={styles.userAvatarWrap}>
-         <Image source={null} style={styles.userAvatarImage}/>
-         <TouchableOpacity style={styles.userAvatarButton} onPress={() => Alert.alert('Add Foto')}>
-            {!isLoadFoto 
+        {image && <Image source={{ uri: image }} style={styles.userAvatarImage} />}
+        <TouchableOpacity style={styles.userAvatarButton} onPress={pickImage}>
+            {!image 
             ? <AntDesign name="pluscircleo" size={25} color="#FF6C00"/>
             : <AntDesign name="closecircleo" size={25} color="#E8E8E8" />}
           
