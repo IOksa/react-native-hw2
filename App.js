4d239d96390146701af97f9cdedf2c394310deec
IOksa <117656767+IOksa@users.noreply.github.com>
2023-08-10 18:34:11 +0300

    update

--- App.js
+++ App.js
@@ -18,8 +18,8 @@ export default function App() {
   return (
     
     <>
-    {/* <RegistrationScreen/> */}
-   <LoginScreen/>
+    <RegistrationScreen/>
+   {/* <LoginScreen/> */}
      {/* <PostsScreen/> */}
     <StatusBar style="auto" />
     </>
